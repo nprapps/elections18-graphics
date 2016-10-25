3bf801090622d7b7d50954ac37a17e9b4f9c7a7f
ClintonKing <clintonjoking@gmail.com>
2016-10-25 15:16:53 -0400

    Basic framework

--- www/js/state-results.js
+++ www/js/state-results.js
@@ -20,7 +20,7 @@ var onWindowLoaded = function() {
         polling: 100
     });
     projector.append(resultsWrapper, renderMaquette);
-    currentState = 'pa';
+    currentState = 'nc';
     const dataFilename = 'presidential-' + currentState + '-counties.json'
     dataURL = buildDataURL(dataFilename);
     getData();
@@ -37,59 +37,120 @@ const getData = function() {
 const renderMaquette = function() {
     if (data) {
         console.log(data);
+
+        let trumpTotal = 0;
+        let trumpPct = 0;
+        let clintonTotal = 0;
+        let clintonPct = 0;
+        for (var fipscode in data){
+            let trump = data[fipscode][0];
+            let clinton = data[fipscode][1];
+            trumpTotal += trump.votecount;
+            clintonTotal += clinton.votecount;
+            trumpPct += trump.votepct;
+            clintonPct += clinton.votepct;
+        }
+        console.log(trumpTotal);
+        console.log(clintonTotal);
+        console.log(trumpPct);
+        console.log(clintonPct);
+
         return h('div.results', ['Loaded',
-          h('div.overall-state', [
-            h('h3', 'Overall State Results'),
+          h('div.state-results', [
+            h('h2', 'Overall State Results'),
             h('p', 'Lorem Ipsum dolemoar usted.'),
             h('h4', '2016'),
             h('table', [
-              h('tr', [
-                h('th', 'Candidate'),
-                h('th', 'Party'),
-                h('th', 'Votes'),
-                h('th', 'Pct')
+              h('thead', [
+                h('tr', [
+                  h('th', 'Candidate'),
+                  h('th', 'Votes'),
+                  h('th', 'Pct')
+                ])
               ]),
               h('tr', [
-                h('td', 'Donald Trump'),
-                h('td', 'Republican'),
-                h('td', 'Number'),
-                h('td', 'Percent')
+                h('td', 'Donald Trump (R)'),
+                h('td', trumpTotal),
+                h('td', trumpPct.toFixed(2) + '%')
               ]),
               h('tr', [
-                h('td', 'Hillary Clinton'),
-                h('td', 'Democrat'),
-                h('td', 'Number'),
-                h('td', 'Percent')
+                h('td', 'Hillary Clinton (D)'),
+                h('td', clintonTotal),
+                h('td', clintonPct.toFixed(2) + '%')
               ])
             ]),
             h('h4', '2012'),
             h('table', [
-              h('tr', [
-                h('th', 'Candidate'),
-                h('th', 'Party'),
-                h('th', 'Votes'),
-                h('th', 'Pct')
+              h('thead', [
+                h('tr', [
+                  h('th', 'Candidate'),
+                  h('th', 'Votes'),
+                  h('th', 'Pct')
+                ])
               ]),
               h('tr', [
-                h('td', 'Mitt Romney'),
-                h('td', 'Republican'),
+                h('td', 'Mitt Romney (R)'),
                 h('td', 'Number'),
                 h('td', 'Percent')
               ]),
               h('tr', [
-                h('td', 'Barack Obama'),
-                h('td', 'Democrat'),
+                h('td', 'Barack Obama (D)'),
                 h('td', 'Number'),
                 h('td', 'Percent')
               ])
             ])
           ]),
+          h('div.county-results', [
+            h('h2', 'Counties To Watch'),
+            h('p', 'Lorem Ipsum blah blah blah'),
+            h('h2', 'All Results'),
+            h('table', [
+              h('thead', [
+                h('tr', [
+                  h('th.county', 'County'),
+                  h('th.vote', 'Trump'),
+                  h('th.vote', 'Clinton')
+                ])
+              ]),
+              h('tbody', [
+                Object.keys(data).map(fipscode => renderCountyRow(data[fipscode]))
+
+              ])
+            ])
+          ])
         ]);
     } else {
         return h('div.results');
     }
 }
 
+const renderCountyRow = function(results){
+  const trump = results[0];
+  const clinton = results[1];
+
+  return h('tr', [
+    h('td', trump.reportingunitname),
+    h('td', trump.votecount),
+    h('td', clinton.votecount)
+  ])
+
+  // if (data){
+  //   for (var fipscode in data){
+  //     let trump = data[fipscode][0];
+  //     let clinton = data[fipscode][1];
+  //     let trumpVotes = trump.votecount;
+  //     let clintonVotes = clinton.votecount;
+  //     let countyName = trump.reportingunitname;
+  //
+  //     return [ h('tr', [
+  //       h('td', countyName),
+  //       h('td', trumpVotes),
+  //       h('td', clintonVotes)
+  //     ]),
+  //   ]
+  //   }
+  // }
+}
 
 /*
  * Initially load the graphic
