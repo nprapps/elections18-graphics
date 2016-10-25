22a91b294e6765a4b587554a95fc681a9f17ef9b
lindamood <wesley.lindamood@gmail.com>
2016-10-25 16:50:26 -0400

    updating height of pym when menu is opened

--- www/js/includes/navbar.js
+++ www/js/includes/navbar.js
@@ -1,1 +1,19 @@
-console.log('the navbar is being loaded in');
+
+
+var resultsMenu = document.querySelector(".small-screen-nav-label");
+var stateMenu = document.querySelector(".state-nav-label");
+var pymChild = null;
+
+resultsMenu.addEventListener("click", function () {
+    // Update iframe
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
+});
+
+stateMenu.addEventListener("click", function () {
+    // Update iframe
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
+});
