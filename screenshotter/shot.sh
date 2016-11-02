3316a08cd4784ef6f1aa0bcdccfcf5a88602bbb9
David Eads <davideads@gmail.com>
2016-11-01 23:22:23 -0400

    write to correct dir and file

--- screenshotter/shot.sh
+++ screenshotter/shot.sh
@@ -2,8 +2,8 @@
 
 export QT_QPA_PLATFORM=offscreen
 FILEDATE=`date +%Y-%m-%d-%H\:%M`
-FILEDIR=/home/ubuntu/screenshots/stage/map/
+FILEDIR=/home/ubuntu/screenshots/stage/map
 
 mkdir -p $FILEDIR
 
-phantomjs `pwd`/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDATE-election16-map.png
+phantomjs `pwd`/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDIR/$FILEDATE-election16-map.png
