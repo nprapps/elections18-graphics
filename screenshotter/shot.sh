fba22a38be6707906c9cab7567200ed4735ae0eb
David Eads <davideads@gmail.com>
2016-11-01 23:17:45 -0400

    make script a little nicer

--- screenshotter/shot.sh
+++ screenshotter/shot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 export QT_QPA_PLATFORM=offscreen
-phantomjs `pwd`/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" /tmp/test.png
+FILEDATE=`date +%Y-%m-%d-%H\:%M`
+FILEDIR=/home/ubuntu/screenshots/stage/map/
+
+mkdir -p $FILEDIR
+
+phantomjs `pwd`/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDATE-election16-map.png
