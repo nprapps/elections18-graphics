07610820acc31b46ddd25b092637e447a1c8a4f8
David Eads <davideads@gmail.com>
2016-11-01 23:35:08 -0400

    be more explicit about path

--- screenshotter/shot.sh
+++ screenshotter/shot.sh
@@ -3,7 +3,8 @@
 export QT_QPA_PLATFORM=offscreen
 FILEDATE=`date +%Y-%m-%d-%H\:%M`
 FILEDIR=/home/ubuntu/screenshots/stage/map
+SCRIPTDIR=/home/ubuntu/elections16graphics/repository
 
 mkdir -p $FILEDIR
 
-phantomjs `pwd`/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDIR/$FILEDATE-election16-map.png
+phantomjs $SCRIPTDIR/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDIR/$FILEDATE-election16-map.png
