9f36060fc41c7e5b163eac3744b3eb88105b51cd
Alyson Hurt <ahurt@npr.org>
2016-11-03 18:29:45 -0400

    account for a third-party candidate in the sidebar graphic #41

--- www/js/bop-sidebar.js
+++ www/js/bop-sidebar.js
@@ -1,36 +1,16 @@
 // npm libraries
 import bop from '../js/includes/bop.js';
-import request from 'superagent';
+import electoral from '../js/includes/electoral-totals.js';
 
 // Global vars
 window.pymChild = null;
-var DATA_FILE = 'top-level-results.json';
-var LOAD_INTERVAL = 15000;
 var isBopInit = false;
-var isMobile = false;
-var electoralData = [];
-var lastRequestTime = null;
-var reloadElectoralData = null;
-
-var electoralTotals = null;
-var clintonTitle = null;
-var clintonElectoral = null;
-var trumpTitle = null;
-var trumpElectoral = null;
-
+var isElectoralInit = false;
 
 /*
 * Initialize the graphic.
 */
 var onWindowLoaded = function() {
-    electoralTotals = d3.select('#electoral-totals');
-    clintonTitle = electoralTotals.select('.clinton h3');
-    clintonElectoral = electoralTotals.select('.clinton-electoral');
-    trumpTitle = electoralTotals.select('.trump h3');
-    trumpElectoral = electoralTotals.select('.trump-electoral');
-
-    loadElectoralData();
-
     // init pym and render callback
     pymChild = new pym.Child({
         renderCallback: render
@@ -47,65 +27,22 @@ var onWindowLoaded = function() {
 
 
 /*
- * Load data
- */
-var loadElectoralData = function() {
-    clearInterval(reloadElectoralData);
-    // console.log('loadData: ' + DATA_FILE);
-    request.get(buildDataURL(DATA_FILE))
-        .set('If-Modified-Since', lastRequestTime ? lastRequestTime : '')
-        .end(function(err, res) {
-            if (err) {
-                console.warn(err);
-            }
-
-            lastRequestTime = new Date().toUTCString();
-            electoralData = res.body.electoral_college;
-            formatElectoralData();
-        });
-}
-
-
-/*
  * Render
  */
 var render = function(containerWidth) {
     // only run the first time
     if (!isBopInit) {
         bop.initBop(containerWidth);
-        isBopInit= true;
+        isBopInit = true;
     // run onresize
     } else {
         bop.renderBop(containerWidth);
     }
-}
 
-
-/*
- * Format data for D3.
- */
-var formatElectoralData = function() {
-    var parties = {
-        'senate': [ 'Dem', 'Other', 'Not yet called', 'GOP' ],
-        'house': [ 'Dem', 'Not yet called', 'Other', 'GOP' ]
+    if (!isElectoralInit) {
+        electoral.initElectoralTotals()
+        isElectoralInit = true;
     }
-
-    // update overall totals
-    trumpElectoral.html(electoralData['Trump']);
-    if (electoralData['Trump'] >= 270) {
-        trumpTitle.html('Trump <i class="icon icon-ok"></i>');
-    } else {
-        trumpTitle.html('Trump');
-    }
-
-    clintonElectoral.html(electoralData['Clinton']);
-    if (electoralData['Clinton'] >= 270) {
-        clintonTitle.html('Clinton <i class="icon icon-ok"></i>');
-    } else {
-        clintonTitle.html('Clinton');
-    }
-
-    reloadElectoralData = setInterval(loadElectoralData, LOAD_INTERVAL);
 
     // Update iframe
     if (window.pymChild) {
