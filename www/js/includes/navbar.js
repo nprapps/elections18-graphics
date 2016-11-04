9894d6bd38ef124ee34466d618df988dd1785053
David Eads <davideads@gmail.com>
2016-11-04 07:36:11 -0400

    add pjax trigger

--- www/js/includes/navbar.js
+++ www/js/includes/navbar.js
@@ -1,6 +1,16 @@
-var resultsMenu = document.querySelector(".small-screen-nav-label");
-var stateMenuButton = document.querySelector(".state-nav-label");
-var stateMenu = document.querySelector(".state-nav .states");
+import URL from 'url-parse';
+
+const parseParentURL = function() {
+    if (!pymChild) {
+        return null;
+    }
+    const parentUrl = new URL(window.pymChild.parentUrl, location, true);
+    if (parentUrl.hostname == '127.0.0.1') {
+        return 'localhost';
+    } else {
+        return parentUrl.hostname.split('.').slice(-2).join('.');
+    }
+}
 
 const updateMenuParent = function(e) {
     // Update iframe
@@ -9,19 +19,31 @@ const updateMenuParent = function(e) {
     }
 }
 
-const followStateMenuLink = function(e) {
-    if (pymChild && pymChild.parentUrl) {
-        if (e.target !== e.currentTarget) {
-            console.log(e.target);
-            console.log(pymChild);
+const followNavLink = function(e) {
+    if (e.target !== e.currentTarget) {
+        if (pymChild) {
+            const domain = parseParentURL();
+            if (domain == 'npr.org') {
+                const seamusid = e.target.getAttribute('data-seamus-id');
+                const url = 'http://www-s1.npr.org/templates/story/story.php?storyId=' + seamusid;
+                pymChild.sendMessage('navigate', url);
+            } else {
+                pymChild.navigateParentTo(e.target.getAttribute('data-relative-href'));
+            }
+        } else {
+            window.location.href = e.target.getAttribute('data-relative-href');
         }
-        //window.location.href = e.target.getAttribute('data-seamusid');
-    } else {
-        window.location.href = e.target.getAttribute('data-relative-href');
     }
+    e.preventDefault();
     e.stopPropagation();
 }
 
-resultsMenu.addEventListener("click", updateMenuParent);
+var resultsMenuButton = document.querySelector(".small-screen-nav-label");
+var resultsMenu = document.querySelector(".menu");
+resultsMenuButton.addEventListener("click", updateMenuParent);
+resultsMenu.addEventListener("click", followNavLink);
+
+var stateMenuButton = document.querySelector(".state-nav-label");
+var stateMenu = document.querySelector(".state-nav .states");
+stateMenu.addEventListener("click", followNavLink);
 stateMenuButton.addEventListener("click", updateMenuParent);
-stateMenu.addEventListener("click", followStateMenuLink);
