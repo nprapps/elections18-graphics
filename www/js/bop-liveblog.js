d382f3ee5cdb4fb93dec03de5a47a72ff69b4d0f
Tyler Fisher <tyler@tylerjfisher.com>
2016-11-05 12:47:53 -0400

    bop-liveblog graphic

---
+++ www/js/bop-liveblog.js
@@ -0,0 +1,58 @@
+// npm libraries
+import bop from '../js/includes/bop.js';
+import electoral from '../js/includes/electoral-totals.js';
+
+// Global vars
+window.pymChild = null;
+var isBopInit = false;
+var isElectoralInit = false;
+
+/*
+* Initialize the graphic.
+*/
+var onWindowLoaded = function() {
+    // init pym and render callback
+    pymChild = new pym.Child({
+        renderCallback: render
+    });
+
+    // pymChild.onMessage('on-screen', function(bucket) {
+    //     ANALYTICS.trackEvent('on-screen', bucket);
+    // });
+    // pymChild.onMessage('scroll-depth', function(data) {
+    //     data = JSON.parse(data);
+    //     ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
+    // });
+}
+
+
+/*
+ * Render
+ */
+var render = function(containerWidth) {
+    // only run the first time
+    if (!isBopInit) {
+        bop.initBop(containerWidth);
+        isBopInit = true;
+    // run onresize
+    } else {
+        bop.renderBop(containerWidth);
+    }
+
+    if (!isElectoralInit) {
+        electoral.initElectoralTotals()
+        isElectoralInit = true;
+    }
+
+    // Update iframe
+    if (window.pymChild) {
+        window.pymChild.sendHeight();
+    }
+}
+
+
+/*
+ * Initially load the graphic
+ * (NB: Use window.load to ensure all images have loaded)
+ */
+window.onload = onWindowLoaded;
