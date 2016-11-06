c0a3b5cd5a8b7a2a201c85149f862d4368d5ea5f
David Eads <davideads@gmail.com>
2016-11-05 22:24:03 -0400

    upload to aws + dropbox

--- screenshotter/shot.sh
+++ screenshotter/shot.sh
@@ -3,20 +3,26 @@
 export QT_QPA_PLATFORM=offscreen
 FILEDATE=`date +%Y-%m-%d-%H\:%M`
 FILEDIR=/home/ubuntu/screenshots/stage/map
+FILENAME="election16-map-$FILEDATE.png"
 SCRIPTDIR=/home/ubuntu/apps/elections16graphics/repository
 
 echo "screenshotting map at $FILEDATE"
 mkdir -p $FILEDIR
 LASTFILE=`ls -t $FILEDIR | head -1`
 
-phantomjs $SCRIPTDIR/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDIR/$FILEDATE-election16-map.png
+
+phantomjs $SCRIPTDIR/screenshotter/screenshot.js "http://stage-apps.npr.org/elections16graphics/map-election-results-standalone/child.html?initialWidth=600&screenshot=1" $FILEDIR/$FILENAME
 
 if [ $LASTFILE ]; then
-    NEWMD5=`md5sum $FILEDIR/$FILEDATE-election16-map.png | awk '{ print $1 }'`
+    NEWMD5=`md5sum $FILEDIR/$FILENAME | awk '{ print $1 }'`
     OLDMD5=`md5sum $FILEDIR/$LASTFILE | awk '{ print $1 }'`
 
     if [ $NEWMD5 = $OLDMD5 ]; then
-        echo "duplicate of $FILEDATE, deleting"
-        rm $FILEDIR/$FILEDATE-election16-map.png
+        echo "duplicate of $LASTFILE, deleting"
+        rm $FILEDIR/$FILENAME
+    else
+        aws-cli s3 cp $FILEDIR/$FILENAME s3://stage-apps.npr.org/elections16graphics/assets/map/latest.png
+        aws-cli s3 cp $FILEDIR/$FILENAME.png s3://stage-apps.npr.org/elections16graphics/assets/map/$FILENAME.png
+        $SCRIPTDIR/dropbox_uploader.sh $FILEDIR/$FILENAME $FILENAME
     fi
 fi
