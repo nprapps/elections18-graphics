f988dea31216ebafab4b4bdc0ef5cd6a81ff1608
Tyler Fisher <tfisher@npr.org>
2016-11-08 16:26:57 -0500

    fix titlecase logic

--- www/js/state-results.js
+++ www/js/state-results.js
@@ -825,7 +825,7 @@ const onRatingClick = function(e) {
 }
 
 const toTitlecase = function(str) {
-    return str.replace(/\w*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return str.replace(/\w*\s/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
 
