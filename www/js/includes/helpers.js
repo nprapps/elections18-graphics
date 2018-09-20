f44fc62ecd6ea7fed7c44e533b9471044dd53f0b
Miles Wingfield Watkins <miles.w.watkins@gmail.com>
2018-09-19 20:52:05 -0400

    Lint and remove unused helper functions

--- www/js/includes/helpers.js
+++ www/js/includes/helpers.js
@@ -9,75 +9,38 @@
  * NOTE: This implementation must be consistent with the Python classify
  * function defined in base_filters.py.
  */
-const classify = function(str) {
-    return str.toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
-}
-
-/*
- * Convert key/value pairs to a style string.
- */
-const formatStyle = function(props) {
-    var s = '';
-
-    for (var key in props) {
-        s += key + ': ' + props[key].toString() + '; ';
-    }
-
-    return s;
-}
-
-/*
- * Create a SVG tansform for a given translation.
- */
-const makeTranslate = function(x, y) {
-    var transform = d3.transform();
-
-    transform.translate[0] = x;
-    transform.translate[1] = y;
-
-    return transform.toString();
-}
+const classify = function (str) {
+  return str.toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w-]+/g, '') // Remove all non-word chars
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+};
 
 /*
  * Parse a url parameter by name.
  * via: http://stackoverflow.com/a/901144
  */
-const getParameterByName = function(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+const getParameterByName = function (name) {
+  name = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+  const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  const results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
 
-/*
- * Convert a url to a location object.
- */
-const urlToLocation = function(url) {
-    var a = document.createElement('a');
-    a.href = url;
-    return a;
-}
-
-const buildDataURL = function(filename) {
-    if (document.location.hostname === '127.0.0.1' ||
-        document.location.hostname === 'localhost' ||
-        document.location.hostname === '0.0.0.0') {
-        return document.location.protocol + '//' + document.location.hostname + ':' + document.location.port + '/data/' + filename;
-    } else {
-        return document.location.protocol + '//' + document.location.hostname + '/elections18-graphics/data/' + filename;
-    }
-}
+const buildDataURL = function (filename) {
+  if (document.location.hostname === '127.0.0.1' ||
+      document.location.hostname === 'localhost' ||
+      document.location.hostname === '0.0.0.0') {
+    return document.location.protocol + '//' + document.location.hostname + ':' + document.location.port + '/data/' + filename;
+  } else {
+    return document.location.protocol + '//' + document.location.hostname + '/elections18-graphics/data/' + filename;
+  }
+};
 
 export {
-    classify,
-    formatStyle,
-    makeTranslate,
-    getParameterByName,
-    urlToLocation,
-    buildDataURL
+  classify,
+  getParameterByName,
+  buildDataURL
 };
