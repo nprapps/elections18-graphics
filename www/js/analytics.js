ce207bf6b8a5cdc58072aa375ad6b3a8562efe56
Miles Wingfield Watkins <miles.w.watkins@gmail.com>
2018-09-27 16:37:52 -0400

    Don't use heavier debug GA JS library

--- www/js/analytics.js
+++ www/js/analytics.js
@@ -21,7 +21,7 @@ window.ANALYTICS = (function () {
             i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics_debug.js','ga');
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
     }
 
     var setupVizAnalytics = function() {
