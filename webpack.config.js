325cd8990dd234e2540b7902c4aa43170e30bd2a
Miles Wingfield Watkins <miles.w.watkins@gmail.com>
2018-10-19 13:01:18 -0400

    By default, don't load the bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = Object.assign(
     devtool: 'eval-source-map',
     watch: true,
     plugins: [
-      new BundleAnalyzerPlugin()
+      // Uncomment this in order to see a bundle-size analyzer,
+      // to help determine which libraries or chunks are contributing
+      // most to the size of the built, deployed JS files
+      // new BundleAnalyzerPlugin()
     ]
   }
 );
